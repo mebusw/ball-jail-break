c08c085a4b4739105c90af5b188390866655aa7d
mebusw <mebusw@163.com>
2012-07-08 15:26:13 +0800

    Initial Commit

---
+++ ball-jail-break/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  ball-jail-break
+//
+//  Created by  on 12-7-8.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ ball-jail-break/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  ball-jail-break
+//
+//  Created by  on 12-7-8.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
